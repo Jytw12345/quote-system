af32651f2df23b2d5599c43918dfb19128e4d1e1
Jytw12345 <309953160@qq.com>
2026-05-31 13:33:38 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
 const CACHE_NAME = 'quote-app-v1';
-const urlsToCache = [
-    './',
-    './index.html'
-];
+
+const selfOrigin = self.location.origin;
+const basePath = '/quote-system/';
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                return cache.addAll(urlsToCache);
+                return cache.addAll([
+                    basePath,
+                    basePath + 'index.html',
+                    basePath + 'manifest.json',
+                    basePath + 'sw.js'
+                ]);
             })
     );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
@@ -20,22 +25,35 @@ self.addEventListener('fetch', event => {
                 if (response) {
                     return response;
                 }
-                return fetch(event.request);
+                return fetch(event.request).then(fetchResponse => {
+                    if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+                        return fetchResponse;
+                    }
+                    const responseToCache = fetchResponse.clone();
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return fetchResponse;
+                });
+            })
+            .catch(() => {
+                return caches.match(basePath + 'index.html');
             })
     );
 });
 
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                    if (cacheName !== CACHE_NAME) {
                         return caches.delete(cacheName);
                     }
                 })
             );
         })
     );
+    self.clients.claim();
 });
